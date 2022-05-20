d99fcbb1bd4ae7ee992685b96c87188f7975610b
jfvandermousen <jeanfrancoisvandermousen@gmail.com>
2022-05-20 10:16:49 +0200

    fetch with redux

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,11 +1,11 @@
-import FilterMoviesList from "../components/FilterMoviesList.jsx";
+import Movies from "../components/Movies.jsx";
 
 
 const MoviesPage = () => {
   return (
     <div>
         <h2 className="text-center text-2xl mb-4">Movies from Studio Ghibli</h2>
-      	  	<FilterMoviesList />
+      	  	<Movies />
     </div>
   )
 }
